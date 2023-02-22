5b8de62bc68203dbbc332bf979c6023347ddf723
Michael Baker <99276772+MikeTB-Microsoft@users.noreply.github.com>
2023-02-22 16:27:00 -0500

    Updating Repo

--- src/aro-workload/VetService/src/main/java/com/springframework/samples/petclinic/vet/URIConstants.java
+++
@@ -1,7 +0,0 @@
-package com.springframework.samples.petclinic.vet;
-
-public class URIConstants {
-	
-	public static final String GET_VETS = "/vets";
-	public static final String GET_VETS_COLLECTION = "/vets/list";		
-}
